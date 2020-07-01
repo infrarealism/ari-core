102a15ddddccd2321bed4aac5cf8a27d9e155452
vauxhall <zero.griffin@gmail.com>
2020-07-01 15:14:52 +0200

    Set id on constructor

--- Sources/Website.swift
+++ Sources/Website.swift
@@ -3,10 +3,11 @@ import Foundation
 public struct Website: Codable, Identifiable {
     public var pages = Set<Page>()
     public var name = ""
-    public let id = UUID()
+    public let id: UUID
     public let category: Category
     
-    public init(category: Category) {
+    public init(_ id: UUID, category: Category) {
+        self.id = id
         self.category = category
     }
 }
