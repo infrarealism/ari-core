9ec3e7802db1c1d95c071f9165ec636c598dfbce
vauxhall <zero.griffin@gmail.com>
2020-07-10 09:41:33 +0200

    Add colors

---
+++ Sources/Color.swift
@@ -0,0 +1,13 @@
+import CoreGraphics
+
+public struct Color {
+    let red: CGFloat
+    let green: CGFloat
+    let blue: CGFloat
+    
+    public init(red: CGFloat, green: CGFloat, blue: CGFloat) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+    }
+}
