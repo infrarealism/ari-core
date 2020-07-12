9ef54eef7ac415008643fb962a29ee3d36579f0c
vauxhall <zero.griffin@gmail.com>
2020-07-12 14:13:13 +0200

    Make colours double

--- Sources/Color.swift
+++ Sources/Color.swift
@@ -1,4 +1,4 @@
-import CoreGraphics
+import Foundation
 
 public struct Color: Codable, Equatable {
     public struct Dynamic: Codable {
@@ -8,17 +8,17 @@ public struct Color: Codable, Equatable {
     
     public static let white = Color(red: 1, green: 1, blue: 1)
     public static let black = Color(red: 0, green: 0, blue: 0)
-    public static let blue = Color(red: 0.0392156862745098, green: 0.5176470588235295, blue: 1.0)
+    public static let blue = Color(red: 0.0392156862745098, green: 0.5176470588235295, blue: 1)
     public static let indigo = Color(red: 0.3686274509803922, green: 0.3607843137254902, blue: 0.9019607843137255)
     public static let purple = Color(red: 0.7490196078431373, green: 0.35294117647058826, blue: 0.9490196078431372)
-    public static let pink = Color(red: 1.0, green: 0.21568627450980393, blue: 0.37254901960784315)
-    public static let orange = Color(red: 1.0, green: 0.6235294117647059, blue: 0.0392156862745098)
+    public static let pink = Color(red: 1, green: 0.21568627450980393, blue: 0.37254901960784315)
+    public static let orange = Color(red: 1, green: 0.6235294117647059, blue: 0.0392156862745098)
     
-    public let red: CGFloat
-    public let green: CGFloat
-    public let blue: CGFloat
+    public let red: Double
+    public let green: Double
+    public let blue: Double
     
-    public init(red: CGFloat, green: CGFloat, blue: CGFloat) {
+    public init(red: Double, green: Double, blue: Double) {
         self.red = red
         self.green = green
         self.blue = blue
@@ -29,7 +29,7 @@ public struct Color: Codable, Equatable {
     }
 }
 
-private extension CGFloat {
+private extension Double {
     var hex: String {
         {
             $0.count == 1 ? "0\($0)" : $0
