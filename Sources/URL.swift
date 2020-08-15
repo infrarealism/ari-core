5d41ad61e3c3f3cd43b6012baa3c688d515f7351
vauxhall <zero.griffin@gmail.com>
2020-08-15 09:55:39 +0200

    Treat pwid as optional

--- Sources/URL.swift
+++ Sources/URL.swift
@@ -16,7 +16,7 @@ public extension URL {
 #endif
     
     var directory: String? {
-        getpwuid(getuid()).pointee.pw_dir.map {
+        getpwuid(getuid())?.pointee.pw_dir.map {
             FileManager.default.string(withFileSystemRepresentation: $0, length: .init(strlen($0)))
         }.map {
             NSString(string: path.replacingOccurrences(of: $0, with: "~")).abbreviatingWithTildeInPath
